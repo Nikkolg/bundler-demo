b36ed3633aab205ac7b775b38c4e8c788d7f67b6
Nikolay Kolganov <nikolaykolganov7@gmail.com>
2023-10-10 12:44:14 +0300

    Add routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Root } from './Components/Root'
 import App from './App.jsx'
 import './index.css'
+/**
+ * Главная - / - сравнение
+ * Webpack - /webpack
+ * Esbuild - /esbuild
+ * Vite - /vite
+ */
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <App />
+      },
+      {
+        path: 'webpack',
+        element: <></>
+      },
+      {
+        path: 'esbuild',
+        element: <></>
+      },
+      {
+        path: 'vite',
+        element: <></>
+      },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
